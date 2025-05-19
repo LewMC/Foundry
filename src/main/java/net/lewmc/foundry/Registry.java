464ad016cdef657343b439ff41205a7fb9517fcc
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-19 23:33:43 +0100

    Add verbose logging.

--- src/main/java/net/lewmc/foundry/Registry.java
+++ src/main/java/net/lewmc/foundry/Registry.java
@@ -3,7 +3,6 @@ package net.lewmc.foundry;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -43,6 +42,7 @@ public class Registry {
     public boolean command(String command, CommandExecutor executor) {
         if (this.plugin.getCommand(command) != null) {
             Objects.requireNonNull(this.plugin.getCommand(command)).setExecutor(executor);
+            this.verboseAlert("Command", command);
             return true;
         } else {
             new Logger(this.config).severe("Failed to register command " + command + " because it is null.");
@@ -63,6 +63,7 @@ public class Registry {
         for (String command : commands) {
             if (this.plugin.getCommand(command) != null) {
                 Objects.requireNonNull(this.plugin.getCommand(command)).setExecutor(executor);
+                this.verboseAlert("Command", command);
                 success[i] = true;
             } else {
                 new Logger(this.config).severe("Failed to register command " + command + " because it is null.");
@@ -82,16 +83,17 @@ public class Registry {
     public boolean tabCompleter(String command, TabCompleter completer) {
         if (this.plugin.getCommand(command) != null) {
             Objects.requireNonNull(this.plugin.getCommand(command)).setTabCompleter(completer);
+            this.verboseAlert("Tab completer", completer.toString());
             return true;
         } else {
-            new Logger(this.config).severe("Failed to register tab completer " + command + " because it is null.");
+            new Logger(this.config).severe("Failed to register tab completer " + completer.toString() + " because it is null.");
             return false;
         }
     }
 
     /**
      * Registers a Tab Completer with the server, handling any potential issues.
-     * @param commands String - The command label, what users type into chat after the / to execute it.
+     * @param commands  String - The command label, what users type into chat after the / to execute it.
      * @param completer TabCompleter - The tab completer class.
      * @return boolean - Was the tab completer successfully registered?
      */
@@ -103,9 +105,10 @@ public class Registry {
         for (String command : commands) {
             if (this.plugin.getCommand(command) != null) {
                 Objects.requireNonNull(this.plugin.getCommand(command)).setTabCompleter(completer);
+                this.verboseAlert("Tab completer", completer.toString());
                 success[i] = true;
             } else {
-                new Logger(this.config).severe("Failed to register command " + command + " because it is null.");
+                new Logger(this.config).severe("Failed to register tab completer " + completer.toString() + " because it is null.");
                 success[i] = false;
             }
             i++;
@@ -115,11 +118,23 @@ public class Registry {
 
     /**
      * Registers an event with the server, handling any potential issues.
-     * @param event Listener - The event listener class.
+     * @param event     Listener - The event listener class.
      * @return boolean - Was the event successfully registered?
      */
     public boolean event(Listener event) {
         Bukkit.getServer().getPluginManager().registerEvents(event, this.plugin);
+        this.verboseAlert("Event", event.toString());
         return true;
+    }
+
+    /**
+     * Alerts the verbose logs of a registration.
+     * @param type String - The type of registration.
+     * @param asset String - The asset registered.
+     */
+    public void verboseAlert(String type, String asset) {
+        if (this.config.verbose) {
+            new Logger(this.config).info("FoundryRegistry > "+type+" "+asset+" registered.");
+        }
     }
 }
