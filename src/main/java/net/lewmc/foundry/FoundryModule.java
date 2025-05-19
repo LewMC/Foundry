96c570b1fe33174e83e025d4851bfba36f681925
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-19 23:59:09 +0100

    Add Modules.

---
+++ src/main/java/net/lewmc/foundry/FoundryModule.java
@@ -0,0 +1,47 @@
+package net.lewmc.foundry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Essence Module class
+ */
+abstract public class FoundryModule {
+    /**
+     * The plugin
+     */
+    protected JavaPlugin plugin;
+
+    /**
+     * The Foundry Registry
+     */
+    protected Registry reg;
+
+    /**
+     * Executes all other functions.
+     * @param plugin    Reference to the main plugin class.
+     * @param reg       The Foundry Registry.
+     */
+    public FoundryModule(JavaPlugin plugin, Registry reg) {
+        this.plugin = plugin;
+        this.reg = reg;
+
+        this.registerCommands();
+        this.registerEvents();
+        this.registerTabCompleters();
+    }
+
+    /**
+     * Where commands to be registered should be placed.
+     */
+    public abstract void registerCommands();
+
+    /**
+     * Where events to be registered should be placed.
+     */
+    public abstract void registerEvents();
+
+    /**
+     * Where tab completers to be registered should be placed.
+     */
+    public abstract void registerTabCompleters();
+}
