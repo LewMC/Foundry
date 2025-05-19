e6030e256c6547e9230c0f62b17b0782159bb6bc
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-05-19 16:52:33 +0100

    Update FoundryConfig.java

--- src/main/java/net/lewmc/foundry/FoundryConfig.java
+++ src/main/java/net/lewmc/foundry/FoundryConfig.java
@@ -4,11 +4,6 @@ import org.bukkit.plugin.Plugin;
 
 public class FoundryConfig {
     /**
-     * Should Foundry output verbose data?
-     */
-    public boolean verbose;
-
-    /**
      * The plugin's ID
      */
     public String pluginId;
@@ -21,9 +16,8 @@ public class FoundryConfig {
     /**
      * Sets up the FoundryConfig class.
      * @param plugin Plugin - The parent plugin.
-     * @param pluginId String - The plugin ID.
      */
-    public FoundryConfig(Plugin plugin, String pluginId) {
-        this.pluginId = pluginId.toLowerCase();
+    public FoundryConfig(Plugin plugin) {
+        this.pluginId = plugin.getName().toUpperCase();
     }
 }
